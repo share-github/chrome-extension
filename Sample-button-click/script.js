10e6ba5e5c6182b1b24520affa9b4c1dc4f7d719
share-github <for.github.share@gmail.com>
2021-07-11 18:01:05 +0900

    add button click sample

---
+++ Sample-button-click/script.js
@@ -0,0 +1,2 @@
+// https://www.amazon.co.jp/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.co.jp%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=jpflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&
+document.querySelector('#createAccountSubmit').click();
